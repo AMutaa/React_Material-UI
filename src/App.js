3b229f7fe4ea5abcdc835acff3da6e53ed3ec399
AMutaa <piusandru@gmail.com>
2018-09-21 13:33:41 -0400

    button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
     );
   }
 }

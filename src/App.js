eb0edf6e500dc9e806cfa1aef7f1e93e501a0db7
AMutaa <piusandru@gmail.com>
2018-09-22 16:30:56 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
+import projects from './Data.json';
 
 class App extends Component {
   render() {
---
+++ src/ProjectCard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Project extends Component {
+  state = {
+    projects: []
+  }
+
+  async componentDidMount() {
+
+  }
+  render() {
+    return (
+
+  )
+  }
+}
